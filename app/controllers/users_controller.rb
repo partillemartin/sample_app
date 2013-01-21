dae0724c028d76c0e2f336449d81c2db08acdbe0
Martin Påhlsson <martin.pahlsson@gmail.com>
2013-01-21 23:29:48 +0100

    Pass test, continue on 7.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:id])
+    if @user.save
+      #save user
+    else
+      render 'new'
+    end
   end
 end
